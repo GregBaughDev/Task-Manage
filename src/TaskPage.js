8c36e13de8867fa109deab3f70db9509ed2a1df5
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-06-04 15:37:03 +1000

    PWV

--- src/TaskPage.js
+++ src/TaskPage.js
@@ -1,47 +1,47 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import CardHolder from './CardHolder';
 import logo from './img/TMlogo.png';
 import './TaskPage.css';
 
-function TaskPage(){
-    return(
-        <>
-        <header>
-            <img className="head-logo" alt="Task Manage logo" src={logo}></img>
-            <h2>Tasks</h2>
-            <div>
-                <button>New Task</button>
-                <button>Log Out</button>
-            </div>
-        </header>
-        <div id="outer">
-            <div className="card-holder">
-                <div className="holder-header">
-                    <div className="head-item">
-                        <h3>Submitted</h3> 
-                    </div>
-                    <div className="head-item">
-                        <h3>In Progress</h3>
-                    </div>
-                    <div className="head-item">
-                        <h3>QA</h3> 
-                    </div>
-                    <div className="head-item">
-                        <h3>Complete</h3>
-                    </div> 
+class TaskPage extends Component {
+    render(){
+        return(
+            <>
+            <header>
+                <img className="head-logo" alt="Task Manage logo" src={logo}></img>
+                <h2>Tasks</h2>
+                <div>
+                    <button>New Task</button>
+                    <button>Log Out</button>
                 </div>
-                <div className="holder-main">
-                    <CardHolder />
-                    <CardHolder />
-                    <CardHolder />
-                    <CardHolder />
+            </header>
+            <div id="outer">
+                <div className="card-holder">
+                    <div className="holder-header">
+                        <div className="head-item">
+                            <h3>Submitted</h3> 
+                        </div>
+                        <div className="head-item">
+                            <h3>In Progress</h3>
+                        </div>
+                        <div className="head-item">
+                            <h3>QA</h3> 
+                        </div>
+                        <div className="head-item">
+                            <h3>Complete</h3>
+                        </div> 
+                    </div>
+                    <div className="holder-main">
+                        <CardHolder />
+                        <CardHolder />
+                        <CardHolder />
+                        <CardHolder />
+                    </div>
                 </div>
             </div>
-        </div>
-        </>
-    );
-    
-
+            </>
+        );
+    }
 }
 
 export default TaskPage;
---
+++ src/initial-data.js
@@ -0,0 +1,33 @@
+const initialData = {
+    tasks: {
+        'task-1': {id: 'task-1', content: 'First task', datetime: '30.05.21 / 12:45', user: 'Greg'},
+        'task-2': {id: 'task-2', content: 'Second task', datetime: '30.05.21 / 12:45', user: 'Greg'},
+        'task-3': {id: 'task-3', content: 'Third task', datetime: '30.05.21 / 12:45', user: 'Greg'},
+        'task-4': {id: 'task-4', content: 'Forth task', datetime: '30.05.21 / 12:45', user: 'Greg'},
+    },
+    columns: {
+        'column-1': {
+            id: 'column-1',
+            title: 'Submitted',
+            taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+        },
+        'column-2': {
+            id: 'column-2',
+            title: 'In progress',
+            taskIds: [],
+        },
+        'column-3': {
+            id: 'column-3',
+            title: 'QA',
+            taskIds: [],
+        },
+        'column-4': {
+            id: 'column-4',
+            title: 'Complete',
+            taskIds: [],
+        }
+    },
+    columnOrder: ['column-1', 'column-2', 'column-3', 'column-4'],
+};
+
+export default initialData;
